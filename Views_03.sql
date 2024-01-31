7388a7fa5e8201dc518ab1e33775669e6128fc09
T. Tran <ttran375@my.centennialcollege.ca>
2024-01-31 09:56:32 +0000

    Wed, Jan 31, 2024, 9:56 AM +00:00

--- Views_03.sql
+++ Views_03.sql
@@ -1,6 +1,4 @@
--- 3. Create a view called HOMEWORK13 that includes the columns named Col1 and
--- Col2 from the FIRSTATTEMPT table. Make sure the view is created even if the
--- FIRSTATTEMPT table doesn’t exist.
+-- 3. Create a view called HOMEWORK13 that includes the columns named Col1 and Col2 from the FIRSTATTEMPT table. Make sure the view is created even if the FIRSTATTEMPT table doesn’t exist.
 CREATE FORCE VIEW
     HOMEWORK13 AS
 SELECT
